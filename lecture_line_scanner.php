d14022d123d072c9ee9ed64169238c23d8f0f71e
Daisuke Inoue <hibariyashiki@gmail.com>
2016-04-24 22:04:37 +0900

    exited test.

--- lecture_line_scanner.php
+++ lecture_line_scanner.php
@@ -9,6 +9,4 @@ if (0 === preg_match('/^#/', $line)) {
 
 $lecture = $lecture_line->to_lecture();
 $event = $lecture->to_event();
-print_r($event);
-print_r($event->to_json());
-//$event->insert();
+$event->insert();
