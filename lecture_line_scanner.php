366f7f6a2266d39c205d76e50a6440166144225c
Daisuke Inoue <hibariyashiki@gmail.com>
2016-04-24 20:47:30 +0900

    edited lecture_line_scanner.

--- lecture_line_scanner.php
+++ lecture_line_scanner.php
@@ -2,20 +2,11 @@
 
 require('LectureLine.php');
 
-$filename = '/home/daisuke/dat/med/2015_3_2.csv';
-//$filename = '/tmp/bi.csv';
-if(is_readable($filename)) {
-    $lines = file($filename);
-    foreach ($lines as $line) {
-        if (0 === preg_match('/^#/', $line)) {
-            $line_object = new LectureLine(trim($line));
-            $line_objects[] = $line_object;
-        }
-    }
+$line = trim(fgets(STDIN));
+if (0 === preg_match('/^#/', $line)) {
+    $lecture_line = new LectureLine($line);
 }
-//print_r($line_objects->to_event()->insert());
 
-foreach($line_objects as $o) {
-    print_r($o);
-    print_r($o->to_lecture()->to_event()->insert());
-}
+$lecture = $lecture_line->to_lecture();
+$event = $lecture->to_event();
+$event->insert();
