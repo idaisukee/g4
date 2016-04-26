6280fc3fd858fe5cc5821e9cd42fb58a2cb2fa13
Daisuke Inoue <hibariyashiki@gmail.com>
2016-04-26 10:26:49 +0900

    list.php for line oriented.

--- list.php
+++ list.php
@@ -1,9 +1,16 @@
 <?php
+$token_json = shell_exec('sh reflesh.sh');
+//        print_r($token_json);
+$token_array = json_decode($token_json, true);
+//        print_r($token_array);
+$bearer = $token_array['access_token'];
 
-$bearer = exec('cat constants/bearer');
 $str = 'curl -H "Authorization: Bearer '. $bearer . '" https://www.googleapis.com/calendar/v3/calendars/primary/events';
 print $str;
 $obj = shell_exec($str);
 
 $dob = json_decode($obj); // decoded object
-print_r($dob->items[0]->summary);
+$items = $dob->items;
+foreach ($items as $item) {
+    print $item->summary . ' ' . $item->start->dateTime . ' ' .$item->id . "\n";
+}
