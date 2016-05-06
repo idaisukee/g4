a045507eea9f8b41cf239ec08080788fe0ff4220
Daisuke Inoue <hibariyashiki@gmail.com>
2016-05-06 20:54:57 +0900

    event_line_scanner.

---
+++ event_line_scanner.php
@@ -0,0 +1,12 @@
+<?php
+
+require('EventLine.php');
+
+$line = trim(fgets(STDIN));
+if (0 === preg_match('/^#/', $line)) {
+    $event_line = new EventLine($line);
+}
+
+$event = $event_line->to_event();
+print_r($event);
+//$event->insert();
