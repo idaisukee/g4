ef06a609a3787bb2bc604a5fc49de36e11a9019e
Daisuke Inoue <hibariyashiki@gmail.com>
2016-06-11 03:40:56 +0900

    completed list.php

--- list.php
+++ list.php
@@ -1,12 +1,26 @@
 <?php
-$token_json = shell_exec('sh reflesh.sh');
-//        print_r($token_json);
+require_once('refresh.php');
+$q = $argv[1];
+$min = $argv[2];
+$max = $argv[3];
+$token_json = refresh();
 $token_array = json_decode($token_json, true);
-//        print_r($token_array);
 $bearer = $token_array['access_token'];
+$str_bearer = '"Authorization: Bearer '.$bearer.'"';
+$url_base = 'https://www.googleapis.com/calendar/v3';
+$cal_id = 'primary';
+$resource = 'calendars';
+$item = 'events';
+$encoded_q = urlencode($q);
+$str_q = 'q='.$encoded_q;
+$encoded_min = urlencode($min);
+$str_min = 'timeMin='.$encoded_min;
+$encoded_max = urlencode($max);
+$str_max = 'timeMax='.$encoded_max;
 
-$str = 'curl -s -H "Authorization: Bearer '. $bearer . '" https://www.googleapis.com/calendar/v3/calendars/primary/events';
-print $str;
+
+$str = 'curl -s -H '.$str_bearer.' '."'".$url_base.'/'.$resource.'/'.$cal_id.'/'.$item.'?'.$str_q.'&'.$str_max.'&'.$str_min."'";
+
 $obj = shell_exec($str);
 
 $dob = json_decode($obj); // decoded object
