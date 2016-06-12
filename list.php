09e8146c8d10978d8b53e5827c600fc9a1b480c7
Daisuke Inoue <hibariyashiki@gmail.com>
2016-06-12 21:58:39 +0900

    use curl lib in list.php.

--- list.php
+++ list.php
@@ -1,30 +1,49 @@
 <?php
 require_once('refresh.php');
+
 $q = $argv[1];
 $min = $argv[2];
 $max = $argv[3];
+
 $token_json = refresh();
 $token_array = json_decode($token_json, true);
+
 $bearer = $token_array['access_token'];
-$str_bearer = '"Authorization: Bearer '.$bearer.'"';
+$str_bearer = 'Authorization: Bearer '.$bearer;
 $url_base = 'https://www.googleapis.com/calendar/v3';
+
 $cal_id = 'primary';
 $resource = 'calendars';
 $item = 'events';
+
 $encoded_q = urlencode($q);
 $str_q = 'q='.$encoded_q;
+
 $encoded_min = urlencode($min);
 $str_min = 'timeMin='.$encoded_min;
+
 $encoded_max = urlencode($max);
 $str_max = 'timeMax='.$encoded_max;
 
+$url = $url_base.'/'.$resource.'/'.$cal_id.'/'.$item.'?'.$str_q.'&'.$str_max.'&'.$str_min;
 
-$str = 'curl -s -H '.$str_bearer.' '."'".$url_base.'/'.$resource.'/'.$cal_id.'/'.$item.'?'.$str_q.'&'.$str_max.'&'.$str_min."'";
+echo $url;
+$curl = curl_init($url);
+$options = [
+	CURLOPT_HTTPHEADER => [
+		$str_bearer
+	],
+	CURLOPT_HTTPGET => true,
+	CURLOPT_RETURNTRANSFER => true,
+];
+curl_setopt_array($curl, $options);
 
-$obj = shell_exec($str);
+$result = curl_exec($curl);
 
-$dob = json_decode($obj); // decoded object
+
+$dob = json_decode($result); // decoded object
+
 $items = $dob->items;
 foreach ($items as $item) {
-    print $item->summary . ' ' . $item->start->dateTime . ' ' .$item->id . "\n";
+  print $item->summary . ' ' . $item->start->dateTime . ' ' .$item->id . "\n";
 }
