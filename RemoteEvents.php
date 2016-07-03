9312e2768130fcca71f6b75c3655e49242455708
Daisuke Inoue <hibariyashiki@gmail.com>
2016-07-03 16:01:33 +0900

    wip

--- RemoteEvents.php
+++ RemoteEvents.php
@@ -26,59 +26,21 @@ class RemoteEvents
 
 		$dob = json_decode($result); // decoded object
 		$this->dob = $dob;
+		print_r($this->dob);
 	}
 
-	function oper($array)
+	function simple_list()
 	{
-		$q = $array['q'];
-		$min = $array['min'];
-		$max = $array['max'];
-		$calendar_name = $array['calendar_name'];
-		$service = $array['service'];
-		$operation = $array['operation'];
-		$http_method = $array['http_method'];
-		$id = $array['id'];
 
-		$query = http_build_query(
-			[
-				'q' => $q,
-				'timeMin' => $min,
-				'timeMax' => $max,
-			]
-		);
-		
-		$calendar_id = calendar_id($calendar_name);
-
-		$token_json = refresh();
-		$token_array = json_decode($token_json, true);
-		$bearer = $token_array['access_token'];
-		$str_bearer = 'Authorization: Bearer '.$bearer;
-
-		$url_base = 'https://www.googleapis.com/calendar/v3';
-		$url_address = implode('/', [$url_base, $service, $calendar_id, $operation]);
-		if (isset($id)) {
-			$url_address = implode('/', [$url_address, $id]);
+		$dob = $this->dob;
+		print_r($dob);
+		$items = $dob->items;
+		foreach ($items as $item) {
+			print $item->summary . ' ' . $item->start->dateTime . ' ' .$item->id . "\n";
 		}
-		
-		if (isset($query)) {
-			$url = $url_address.'?'.$query;
-		} else {
-			$url = $url_address;
-		}
-		
-		$curl = curl_init($url);
-		$options = [
-			CURLOPT_HTTPHEADER => [
-				$str_bearer
-			],
-			CURLOPT_CUSTOMREQUEST => $http_method,
-			CURLOPT_RETURNTRANSFER => true,
-		];
-		curl_setopt_array($curl, $options);
-
-		$result = curl_exec($curl);
-		return $result;
 	}
+
+
 
 }
 
