28cc5a96a53efce83e39c3d8d90c59aa289deee6
Daisuke Inoue <hibariyashiki@gmail.com>
2016-11-11 15:18:45 +0900

    calc

--- calc.sh
+++ calc.sh
@@ -2,6 +2,8 @@
 dir=`pwd`
 Q=$1
 m=$2
+n=$3
+
 cd $PRD/src/g4
-php list.php 2> /dev/null -q $Q -m $m -n pri | cut -d' ' -f 3 | while read line; do echo $line| php get.php 2> /dev/null | cut -d ' ' -f3,4 ;done  | while read line; do echo $line | ruby time.rb; done | xargs | sed 's/ /+/g' | bc
+php list.php 2> /dev/null -q $Q -m $m -n $n | cut -d' ' -f 3 | while read line; do echo $line| php get.php 2> /dev/null | cut -d ' ' -f3,4 ;done  | while read line; do echo $line | ruby time.rb; done | xargs | sed 's/ /+/g' | bc
 cd $dir
