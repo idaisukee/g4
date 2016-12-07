fad669dc5dc143174e7c8767786d0ff72646e56f
Daisuke Inoue <hibariyashiki@gmail.com>
2016-12-07 14:47:32 +0900

    cbt_sum

---
+++ cbt_sum.rb
@@ -0,0 +1,23 @@
+file = ENV['PRD'] + '/dat/cbt/cbt.dat'
+
+ar = Array.new
+sum = 0.0
+ratio_sum = 0
+File.open(file) do |file|
+	file.each_line do |line|
+		child_ar =  line.split(' ')
+		ar << child_ar
+		time = child_ar[1].to_f
+		sum += time
+	end
+end
+
+ar.each do |line|
+	time = line[1].to_f
+	ratio = time / sum
+	ratio_sum += ratio
+	puts [line[0],
+				 (line[0].to_i + 9).to_s,
+				 time,
+				 ratio_sum].join(' ')
+end
